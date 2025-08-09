eb03875fcb5d699732940e901208420b462d756b
Sorawit <thankzaki@hotmail.com>
2025-08-09 15:14:42 +0700

    Step 3: Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello Flutter! Step2");
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(50),
+        child: Text(
+          "Hello Flutter! Step3",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
