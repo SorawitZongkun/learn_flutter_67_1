53c62b3cedc0eec2198ab7525719c5d14d378464
Sorawit <thankzaki@hotmail.com>
2025-08-23 10:50:29 +0700

    Step 13: TextFormField, Step 14: DropdownButton

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,26 @@ import 'package:learn_flutter_67_1/screen/home.dart';
 
 import 'package:learn_flutter_67_1/screen/item.dart';
 
+import 'package:learn_flutter_67_1/screen/addForm.dart';
+
 void main() {
   // runApp(const MyApp());
 
   // const app = MaterialApp(title: "หัวข้อ", home: Text("Hello World!"));
   // runApp(app);
-  runApp(
-    MaterialApp(
-      title: "หัวข้อ",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My App"),
-          backgroundColor: Colors.pinkAccent,
-          centerTitle: true,
-        ),
-        // body: Home(),
-        body: Item(),
-      ),
-    ),
-  );
+  // runApp(
+  //   MaterialApp(
+  //     title: "หัวข้อ",
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text("My App"),
+  //         backgroundColor: Colors.pinkAccent,
+  //         centerTitle: true,
+  //       ),
+  //       // body: Home(),
+  //       body: Item(),
+  //     ),
+  //   ),
+  // );
+  runApp(const AddForm());
 }
